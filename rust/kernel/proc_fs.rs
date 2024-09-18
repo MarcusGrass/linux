2037f3da06a3992dae0b9d1e3de536d5c1f66e3c
MarcusGrass <marcus.grass@protonmail.com>
2024-09-18 10:40:29 +0200

    Nicer abstraction

--- rust/kernel/proc_fs.rs
+++ rust/kernel/proc_fs.rs
@@ -1,45 +1,50 @@
 //! Implementation of proc_fs functionality
 //!
 
-use core::option::Option;
+use core::{marker::PhantomData, option::Option};
 
-use bindings::{file, inode, loff_t};
+use bindings::{file, inode, loff_t, proc_ops};
 
 use kernel::error::Result;
 
-use crate::prelude::{EINVAL, ENOMEM};
+use crate::{
+    prelude::{EINVAL, ENOMEM},
+    uaccess::{UserSlice, UserSliceReader, UserSliceWriter},
+};
 
-/// Proc options builder
-pub struct ProcOpsBuilder {
-    flags: u32,
-    proc_open: Option<unsafe extern "C" fn(arg1: *mut inode, arg2: *mut file) -> core::ffi::c_int>,
-    proc_read: Option<
-        unsafe extern "C" fn(
-            arg1: *mut file,
-            arg2: *mut core::ffi::c_char,
-            arg3: usize,
-            arg4: *mut loff_t,
-        ) -> isize,
-    >,
-    proc_write: Option<
-        unsafe extern "C" fn(
-            arg1: *mut file,
-            arg2: *const core::ffi::c_char,
-            arg3: usize,
-            arg4: *mut loff_t,
-        ) -> isize,
-    >,
-    proc_lseek: Option<
-        unsafe extern "C" fn(arg1: *mut file, arg2: loff_t, arg3: core::ffi::c_int) -> loff_t,
-    >,
+/// Type alias for open function signature
+pub type ProcOpen<'a> = &'a dyn Fn(&inode, &file) -> Result<i32>;
+/// Type alias for read function signature
+pub type ProcRead<'a> = &'a dyn Fn(&file, UserSliceWriter, &loff_t) -> Result<(usize, usize)>;
+/// Type alias for write function signature
+pub type ProcWrite<'a> = &'a dyn Fn(&file, UserSliceReader, &loff_t) -> Result<(usize, usize)>;
+/// Type alias for lseek function signature
+pub type ProcLseek<'a> = &'a dyn Fn(&file, loff_t, Whence) -> Result<loff_t>;
+
+/// Proc file ops handler
+pub trait ProcHandler<'a> {
+    /// Open handler
+    const OPEN: ProcOpen<'a>;
+    /// Read handler
+    const READ: ProcRead<'a>;
+    /// Write handler
+    const WRITE: ProcWrite<'a>;
+    /// Lseek handler
+    const LSEEK: ProcLseek<'a>;
 }
 
+/// Todo: linux doc link
 #[repr(u32)]
 pub enum Whence {
+    /// Todo
     SeekSet = kernel::bindings::SEEK_SET,
+    /// Todo
     SeekCur = kernel::bindings::SEEK_CUR,
+    /// Todo
     SeekEnd = kernel::bindings::SEEK_END,
+    /// Todo
     SeekData = kernel::bindings::SEEK_DATA,
+    /// Todo
     SeekHole = kernel::bindings::SEEK_HOLE,
 }
 
@@ -58,89 +63,151 @@ impl TryFrom<u32> for Whence {
     }
 }
 
-impl ProcOpsBuilder {
-    /// Create a new builder
-    pub const fn new(flags: u32) -> Self {
+/// Usable wrapper for proc ops
+pub struct ProcOps<'a, T>
+where
+    T: ProcHandler<'a>,
+{
+    ops: bindings::proc_ops,
+    _pd: PhantomData<&'a T>,
+}
+impl<'a, T> ProcOps<'a, T>
+where
+    T: ProcHandler<'a>,
+{
+    /// Create new ProcOps from a handler and flags
+    pub const fn new(proc_flags: u32) -> Self {
         Self {
-            flags,
-            proc_open: None,
-            proc_read: None,
-            proc_write: None,
-            proc_lseek: None,
+            ops: proc_ops {
+                proc_flags,
+                proc_open: Some(ProcOps::<'a, T>::proc_open),
+                proc_read: Some(ProcOps::<'a, T>::proc_read),
+                proc_read_iter: None,
+                proc_write: Some(ProcOps::<'a, T>::proc_write),
+                proc_lseek: Some(ProcOps::<'a, T>::proc_lseek),
+                proc_release: None,
+                proc_poll: None,
+                proc_ioctl: None,
+                proc_compat_ioctl: None,
+                proc_mmap: None,
+                proc_get_unmapped_area: None,
+            },
+            _pd: PhantomData,
         }
     }
-
-    pub const fn nonseekable_open(mut self) -> Self {
-        self.proc_open = Some(nonseekable_open);
-        self
+    unsafe extern "C" fn proc_open(
+        inode: *mut kernel::bindings::inode,
+        file: *mut kernel::bindings::file,
+    ) -> i32 {
+        unsafe {
+            // Todo: This is likely a UB-risk, need a better abstraction than
+            // casting this as a mutable reference, since it's unknown what else
+            // in the kernel may be mutably referencing this.
+            let Some(inode_ref) = inode.as_ref() else {
+                return EINVAL.to_errno();
+            };
+            let Some(file_ref) = file.as_ref() else {
+                return EINVAL.to_errno();
+            };
+            match (T::OPEN)(inode_ref, file_ref) {
+                Ok(code) => code,
+                Err(e) => e.to_errno(),
+            }
+        }
     }
-
-    /// Add an on-open callback
-    pub const fn with_open(
-        mut self,
-        func: unsafe extern "C" fn(arg1: *mut inode, arg2: *mut file) -> core::ffi::c_int,
-    ) -> Self {
-        self.proc_open = Some(func);
-        self
+    unsafe extern "C" fn proc_read(
+        file: *mut kernel::bindings::file,
+        buf: *mut core::ffi::c_char,
+        buf_cap: usize,
+        read_offset: *mut kernel::bindings::loff_t,
+    ) -> isize {
+        let file_ref = unsafe {
+            match file.as_ref() {
+                Some(f) => f,
+                None => {
+                    return EINVAL.to_errno() as isize;
+                }
+            }
+        };
+        let buf = buf as *mut u8 as usize;
+        let buf_ref = UserSlice::new(buf, buf_cap);
+        let buf_writer = buf_ref.writer();
+        let offset = unsafe {
+            let Some(offset_ref) = read_offset.as_mut() else {
+                return EINVAL.to_errno() as isize;
+            };
+            offset_ref
+        };
+        match (T::READ)(file_ref, buf_writer, offset) {
+            // Todo: Double check this conversion, only 'safe' if in large file mode
+            Ok((read_bytes, next_offset)) => {
+                unsafe {
+                    read_offset.write(next_offset as i64);
+                }
+                read_bytes as isize
+            }
+            Err(e) => e.to_errno() as isize,
+        }
     }
-
-    /// Add an on-read callback
-    pub const fn with_read(
-        mut self,
-        func: unsafe extern "C" fn(
-            arg1: *mut file,
-            arg2: *mut core::ffi::c_char,
-            arg3: usize,
-            arg4: *mut loff_t,
-        ) -> isize,
-    ) -> Self {
-        self.proc_read = Some(func);
-        self
+    unsafe extern "C" fn proc_write(
+        file: *mut kernel::bindings::file,
+        buf: *const core::ffi::c_char,
+        buf_cap: usize,
+        write_offset: *mut kernel::bindings::loff_t,
+    ) -> isize {
+        let file_ref = unsafe {
+            match file.as_ref() {
+                Some(f) => f,
+                None => {
+                    return EINVAL.to_errno() as isize;
+                }
+            }
+        };
+        let buf = buf as *mut u8 as usize;
+        let buf_ref = UserSlice::new(buf, buf_cap);
+        let buf_writer = buf_ref.reader();
+        let offset = unsafe {
+            let Some(offset_ref) = write_offset.as_mut() else {
+                return EINVAL.to_errno() as isize;
+            };
+            offset_ref
+        };
+        match (T::WRITE)(file_ref, buf_writer, offset) {
+            // Todo: Double check this conversion, only 'safe' if in large file mode
+            Ok((written_bytes, next_offset)) => {
+                unsafe {
+                    write_offset.write(next_offset as i64);
+                }
+                written_bytes as isize
+            }
+            Err(e) => e.to_errno() as isize,
+        }
     }
-
-    /// Add an on-write callback
-    pub const fn with_write(
-        mut self,
-        func: unsafe extern "C" fn(
-            arg1: *mut file,
-            arg2: *const core::ffi::c_char,
-            arg3: usize,
-            arg4: *mut loff_t,
-        ) -> isize,
-    ) -> Self {
-        self.proc_write = Some(func);
-        self
-    }
-
-    pub const fn with_lseek(
-        mut self,
-        func: unsafe extern "C" fn(arg1: *mut file, arg2: loff_t, arg3: core::ffi::c_int) -> loff_t,
-    ) -> Self {
-        self.proc_lseek = Some(func);
-        self
-    }
-
-    /// Construct a usable ProcOps-wrapper
-    pub const fn into_proc_ops(self) -> ProcOps {
-        ProcOps(bindings::proc_ops {
-            proc_flags: self.flags,
-            proc_open: self.proc_open,
-            proc_read: self.proc_read,
-            proc_read_iter: None,
-            proc_write: self.proc_write,
-            proc_lseek: self.proc_lseek,
-            proc_release: None,
-            proc_poll: None,
-            proc_ioctl: None,
-            proc_compat_ioctl: None,
-            proc_mmap: None,
-            proc_get_unmapped_area: None,
-        })
+    unsafe extern "C" fn proc_lseek(
+        file: *mut kernel::bindings::file,
+        offset: kernel::bindings::loff_t,
+        whence: core::ffi::c_int,
+    ) -> kernel::bindings::loff_t {
+        let Ok(whence_u32) = u32::try_from(whence) else {
+            return EINVAL.to_errno().into();
+        };
+        let Ok(whence) = Whence::try_from(whence_u32) else {
+            return EINVAL.to_errno().into();
+        };
+        let file_ref = unsafe {
+            let Some(file_ref) = file.as_ref() else {
+                return EINVAL.to_errno().into();
+            };
+            file_ref
+        };
+        match (T::LSEEK)(file_ref, offset, whence) {
+            core::result::Result::Ok(offs) => offs,
+            core::result::Result::Err(e) => {
+                return e.to_errno().into();
+            }
+        }
     }
 }
-
-/// Usable wrapper for proc ops
-pub struct ProcOps(bindings::proc_ops);
 
 /// A proc directory entry.
 pub struct ProcDirEntry<'a> {
@@ -156,6 +223,12 @@ impl<'a> Drop for ProcDirEntry<'a> {
     }
 }
 
+/// Nonseekable open
+#[inline]
+pub fn rust_nonseekable(i: &mut inode, f: &mut file) -> Result<i32> {
+    unsafe { Ok(nonseekable_open(i as *mut inode, f as *mut file)) }
+}
+
 /// Open as nonseekable, can be used as part of a supplied `unsafe extern "C"` `proc_open`
 #[inline]
 pub unsafe extern "C" fn nonseekable_open(inode: *mut inode, file: *mut file) -> i32 {
@@ -163,13 +236,16 @@ pub unsafe extern "C" fn nonseekable_open(inode: *mut inode, file: *mut file) ->
 }
 
 /// Create a proc entry with the filename `name`
-pub fn proc_create<'a>(
+pub fn proc_create<'a, T>(
     name: &'static kernel::str::CStr,
     mode: bindings::umode_t,
     dir_entry: Option<&ProcDirEntry<'a>>,
-    proc_ops: &'a ProcOps,
-) -> Result<ProcDirEntry<'a>> {
-    let pops = core::ptr::addr_of!(proc_ops.0);
+    proc_ops: &'a ProcOps<'a, T>,
+) -> Result<ProcDirEntry<'a>>
+where
+    T: ProcHandler<'a>,
+{
+    let pops = core::ptr::addr_of!(proc_ops.ops);
     let pde = unsafe {
         let dir_ent = dir_entry
             .map(|de| de.ptr.as_ptr())
--- samples/rust/rust_proc_ram_file.rs
+++ samples/rust/rust_proc_ram_file.rs
@@ -11,12 +11,12 @@ use core::{
 use kernel::{
     c_str,
     prelude::*,
-    proc_fs::{proc_create, ProcDirEntry, ProcOps, ProcOpsBuilder, Whence},
+    proc_fs::{proc_create, ProcDirEntry, ProcHandler, ProcOps, Whence},
     sync::{
         lock::{mutex::MutexBackend, Guard},
         Mutex,
     },
-    uaccess::UserSlice,
+    uaccess::{UserSliceReader, UserSliceWriter},
 };
 
 module! {
@@ -60,12 +60,12 @@ impl SharedRamFile {
         }
     }
 
-    fn write(&self, user: UserSlice, offset: Option<usize>) -> Result<usize> {
+    fn write(&self, user: UserSliceReader, offset: Option<usize>) -> Result<usize> {
         let mut shared_ram = self.lock_buf_inner()?;
         let Some(inner) = shared_ram.as_mut() else {
             return Err(EBUSY);
         };
-        let buf = user.reader();
+        let buf = user;
         let len = buf.len();
         pr_info!("Wants write {len} bytes, offset={offset:?}\n");
 
@@ -99,12 +99,12 @@ impl SharedRamFile {
         Ok(cur.len())
     }
 
-    fn read(&self, buf: UserSlice, offset: usize) -> Result<(usize, usize)> {
+    fn read(&self, buf: UserSliceWriter, offset: usize) -> Result<(usize, usize)> {
         let shared_ram = self.lock_buf_inner()?;
         let Some(inner) = shared_ram.as_ref() else {
             return Err(EBUSY);
         };
-        let mut buf = buf.writer();
+        let mut buf = buf;
         pr_info!("Wants read max {} bytes at offset={offset}\n", buf.len());
         let cur: &[u8] = inner.buf.as_slice();
         let Some(wants_section) = cur.get(offset..) else {
@@ -185,15 +185,71 @@ struct SharedRamInnner {
     _pde: ProcDirEntry<'static>,
 }
 
-const POPS: ProcOps = ProcOpsBuilder::new(0)
-    .with_open(proc_open)
-    .with_read(proc_read)
-    .with_write(proc_write)
-    .with_lseek(proc_lseek)
-    .into_proc_ops();
+struct ProcHand;
+
+impl ProcHandler<'static> for ProcHand {
+    const OPEN: kernel::proc_fs::ProcOpen<'static> = &popen;
+
+    const READ: kernel::proc_fs::ProcRead<'static> = &pread;
+
+    const WRITE: kernel::proc_fs::ProcWrite<'static> = &pwrite;
+
+    const LSEEK: kernel::proc_fs::ProcLseek<'static> = &plseek;
+}
+
+#[inline]
+fn popen(_inode: &kernel::bindings::inode, _file: &kernel::bindings::file) -> Result<i32> {
+    Ok(0)
+}
+
+fn pread(
+    _file: &kernel::bindings::file,
+    user_slice: UserSliceWriter,
+    offset: &kernel::bindings::loff_t,
+) -> Result<(usize, usize)> {
+    let Ok(offset) = usize::try_from(*offset) else {
+        return Err(EINVAL);
+    };
+    let (read, next_offset) = FILE_DATA.read(user_slice, offset)?;
+    Ok((read, next_offset))
+}
+
+fn pwrite(
+    file: &kernel::bindings::file,
+    user_slice_reader: UserSliceReader,
+    offset: &kernel::bindings::loff_t,
+) -> Result<(usize, usize)> {
+    let len = user_slice_reader.len();
+    let Ok(offset) = usize::try_from(*offset) else {
+        return Err(EINVAL);
+    };
+    let offset = if file_is_append(file) {
+        None
+    } else {
+        Some(offset)
+    };
+
+    let next_offset = FILE_DATA.write(user_slice_reader, offset)?;
+    Ok((len, next_offset))
+}
+
+fn plseek(
+    file: &kernel::bindings::file,
+    offset: kernel::bindings::loff_t,
+    whence: Whence,
+) -> Result<kernel::bindings::loff_t> {
+    let off = FILE_DATA.lseek(file.f_pos, offset, whence)?;
+    let Ok(output) = kernel::bindings::loff_t::try_from(off) else {
+        // Todo: Should be EOVERFLOW afaik
+        return Err(EINVAL);
+    };
+    Ok(output)
+}
 
 static INITIALIZED: AtomicBool = AtomicBool::new(false);
 static FILE_DATA: SharedRamFile = SharedRamFile::uninit();
+
+const POPS: ProcOps<'static, ProcHand> = ProcOps::<'static, ProcHand>::new(0);
 
 impl kernel::Module for RustProcRamFile {
     fn init(_module: &'static ThisModule) -> Result<Self> {
@@ -219,136 +275,7 @@ impl Drop for RustProcRamFile {
     }
 }
 
-unsafe extern "C" fn proc_open(
-    _inode: *mut kernel::bindings::inode,
-    _file: *mut kernel::bindings::file,
-) -> i32 {
-    0
-}
-
-fn file_is_append(file: *mut kernel::bindings::file) -> Result<bool> {
-    unsafe {
-        let f = file.as_ref().ok_or_else(|| EINVAL)?;
-        Ok(f.f_flags & kernel::bindings::O_APPEND != 0)
-    }
-}
-
-unsafe extern "C" fn proc_read(
-    _file: *mut kernel::bindings::file,
-    buf: *mut core::ffi::c_char,
-    buf_cap: usize,
-    read_offset: *mut kernel::bindings::loff_t,
-) -> isize {
-    let buf = buf as *mut u8 as usize;
-    let buf_ref = UserSlice::new(buf, buf_cap);
-    let offset = unsafe {
-        let Some(offset_ref) = read_offset.as_mut() else {
-            return EINVAL.to_errno() as isize;
-        };
-        offset_ref
-    };
-    let Ok(offset) = usize::try_from(*offset) else {
-        return EINVAL.to_errno() as isize;
-    };
-    let next_offset = FILE_DATA.read(buf_ref, offset);
-    let (read, next_offset) = match next_offset {
-        core::result::Result::Ok(no) => no,
-        core::result::Result::Err(e) => {
-            return e.to_errno() as isize;
-        }
-    };
-    let Ok(next_offset) = kernel::bindings::loff_t::try_from(next_offset) else {
-        return EINVAL.to_errno() as isize;
-    };
-    unsafe {
-        read_offset.write(next_offset);
-    }
-
-    let Ok(ret) = isize::try_from(read) else {
-        return EINVAL.to_errno() as isize;
-    };
-    ret
-}
-
-unsafe extern "C" fn proc_write(
-    file: *mut kernel::bindings::file,
-    buf: *const core::ffi::c_char,
-    buf_cap: usize,
-    write_offset: *mut kernel::bindings::loff_t,
-) -> isize {
-    let buf = buf as *const u8 as usize;
-    let user_buf = UserSlice::new(buf, buf_cap);
-    let offset = unsafe {
-        let Some(offset_ref) = write_offset.as_mut() else {
-            return EINVAL.to_errno() as isize;
-        };
-        offset_ref
-    };
-    let Ok(offset) = usize::try_from(*offset) else {
-        return EINVAL.to_errno() as isize;
-    };
-    let offset = match file_is_append(file) {
-        Ok(is_append) => {
-            if is_append {
-                None
-            } else {
-                Some(offset)
-            }
-        }
-        Err(e) => {
-            return e.to_errno() as isize;
-        }
-    };
-    let next_offset = FILE_DATA.write(user_buf, offset);
-    let next_offset = match next_offset {
-        core::result::Result::Ok(no) => no,
-        core::result::Result::Err(e) => {
-            return e.to_errno() as isize;
-        }
-    };
-    let Ok(next_offset) = kernel::bindings::loff_t::try_from(next_offset) else {
-        return EINVAL.to_errno() as isize;
-    };
-    unsafe {
-        write_offset.write(next_offset);
-    }
-
-    let Ok(ret) = isize::try_from(buf_cap) else {
-        return EINVAL.to_errno() as isize;
-    };
-    ret
-}
-
-unsafe extern "C" fn proc_lseek(
-    file: *mut kernel::bindings::file,
-    offset: kernel::bindings::loff_t,
-    whence: core::ffi::c_int,
-) -> kernel::bindings::loff_t {
-    let Ok(whence_u32) = u32::try_from(whence) else {
-        return EINVAL.to_errno().into();
-    };
-    let Ok(whence) = Whence::try_from(whence_u32) else {
-        return EINVAL.to_errno().into();
-    };
-    let file_ref = unsafe {
-        let Some(file_ref) = file.as_ref() else {
-            return EINVAL.to_errno().into();
-        };
-        file_ref
-    };
-    let off = match FILE_DATA.lseek(file_ref.f_pos, offset, whence) {
-        core::result::Result::Ok(offs) => offs,
-        core::result::Result::Err(e) => {
-            return e.to_errno().into();
-        }
-    };
-    pr_info!(
-        "Seek pos={}, offset={offset}, next_off={off}",
-        file_ref.f_pos
-    );
-    let Ok(output) = kernel::bindings::loff_t::try_from(off) else {
-        // Todo: Should be EOVERFLOW afaik
-        return EINVAL.to_errno().into();
-    };
-    output
+#[inline]
+fn file_is_append(file: &kernel::bindings::file) -> bool {
+    file.f_flags & kernel::bindings::O_APPEND != 0
 }

0e8686672bfe8859ed17d7c82e546d677e3b0e92
MarcusGrass <marcus.grass@protonmail.com>
2024-09-19 11:33:22 +0200

    slightly better abstractions

--- samples/rust/rust_proc_ram_file.rs
+++ samples/rust/rust_proc_ram_file.rs
@@ -2,20 +2,13 @@
 
 //! Rust simple proc file example.
 
-use core::{
-    cell::UnsafeCell,
-    marker::{Send, Sync},
-    sync::atomic::AtomicBool,
-};
+use core::{mem::MaybeUninit, option::Option, sync::atomic::Ordering};
 
 use kernel::{
     c_str,
     prelude::*,
     proc_fs::{proc_create, ProcDirEntry, ProcHandler, ProcOps, Whence},
-    sync::{
-        lock::{mutex::MutexBackend, Guard},
-        Mutex,
-    },
+    sync::Mutex,
     uaccess::{UserSliceReader, UserSliceWriter},
 };
 
@@ -29,43 +22,139 @@ module! {
 
 struct RustProcRamFile;
 
-struct SharedRamFile {
-    data: UnsafeCell<Option<Pin<Box<Mutex<Option<SharedRamInnner>>>>>>,
+static INITIALIZED: core::sync::atomic::AtomicBool = core::sync::atomic::AtomicBool::new(false);
+
+fn is_initialized() -> bool {
+    INITIALIZED.load(Ordering::Acquire)
 }
 
-unsafe impl Send for SharedRamFile {}
-unsafe impl Sync for SharedRamFile {}
+fn set_init() {
+    INITIALIZED.store(true, Ordering::Release);
+}
 
-impl SharedRamFile {
-    const fn uninit() -> Self {
-        Self {
-            data: UnsafeCell::new(None),
-        }
-    }
+fn set_uninit() {
+    INITIALIZED.store(false, Ordering::Release);
+}
 
-    unsafe fn init(&self, data: Pin<Box<Mutex<Option<SharedRamInnner>>>>) {
-        unsafe {
-            self.data.get().write(Some(data));
-        }
-        INITIALIZED.store(true, core::sync::atomic::Ordering::Release);
-    }
+static mut MAYBE_UNUNIT_DATA_SLOT: MaybeUninit<Mutex<Option<SharedRamInnner>>> =
+    MaybeUninit::uninit();
 
-    fn disable(&self) -> Option<SharedRamInnner> {
-        INITIALIZED.store(false, core::sync::atomic::Ordering::Release);
-        let data_ref = unsafe { self.data.get().as_ref().unwrap() };
-        if let Some(inner_data) = data_ref {
-            inner_data.lock().take()
-        } else {
-            None
-        }
-    }
+const POPS: ProcOps<'static, ProcHand> = ProcOps::<'static, ProcHand>::new(0);
 
-    fn write(&self, user: UserSliceReader, offset: Option<usize>) -> Result<usize> {
-        let mut shared_ram = self.lock_buf_inner()?;
-        let Some(inner) = shared_ram.as_mut() else {
-            return Err(EBUSY);
+impl kernel::Module for RustProcRamFile {
+    fn init(_module: &'static ThisModule) -> Result<Self> {
+        let pde = proc_create(c_str!("rust-proc-file"), 0666, None, &POPS)?;
+
+        let sri = SharedRamInnner {
+            buf: alloc::vec::Vec::new(),
+            _pde: pde,
         };
-        let buf = user;
+        let lock = kernel::new_mutex!(Some(sri), "proc_ram_mutex");
+        unsafe {
+            let slot = MAYBE_UNUNIT_DATA_SLOT.as_mut_ptr();
+            lock.__pinned_init(slot)?;
+        }
+        set_init();
+        pr_info!("Loaded /proc/rust-proc-file\n");
+        Ok(Self)
+    }
+}
+
+impl Drop for RustProcRamFile {
+    fn drop(&mut self) {
+        // Any new readers will bounce after this toggle
+        set_uninit();
+        let data_owned = unsafe { MAYBE_UNUNIT_DATA_SLOT.assume_init_ref() };
+        data_owned.lock().take();
+        // There is theoretically a race-condition, where module-users are currently in a
+        // proc handler, the handler itself is 'static, so the kernel will be trusted
+        // to keep function-related memory initialized until it's no longer needed.
+        // There is a race-condition where it's impossible that the file can be removed and it's made sure that all users
+        // get a 'graceful' exit, ie. all users who can see a file and start a proc-op gets to
+        // finish it. This is because the module recording that a user has entered, and removing
+        // the proc-entry can't happen atomically together. It's impossible to ensure that there
+        // isn't a gap between a user entering the proc-handler, then recording its presense, and
+        // removing the proc-entry and checking if the user registered.
+    }
+}
+
+fn with_data<T, F: FnOnce(&mut SharedRamInnner) -> Result<T>>(func: F) -> Result<T> {
+    if !is_initialized() {
+        return Err(EBUSY);
+    }
+    let value = unsafe { MAYBE_UNUNIT_DATA_SLOT.assume_init_ref() };
+    let mut guard = value.lock();
+    if let Some(inner) = guard.as_mut() {
+        (func)(inner)
+    } else {
+        return Err(EBUSY);
+    }
+}
+
+struct SharedRamInnner {
+    buf: alloc::vec::Vec<u8>,
+    _pde: ProcDirEntry<'static>,
+}
+
+struct ProcHand;
+
+impl ProcHandler<'static> for ProcHand {
+    const OPEN: kernel::proc_fs::ProcOpen<'static> = &popen;
+
+    const READ: kernel::proc_fs::ProcRead<'static> = &pread;
+
+    const WRITE: kernel::proc_fs::ProcWrite<'static> = &pwrite;
+
+    const LSEEK: kernel::proc_fs::ProcLseek<'static> = &plseek;
+}
+
+#[inline]
+fn popen(_inode: &kernel::bindings::inode, _file: &kernel::bindings::file) -> Result<i32> {
+    Ok(0)
+}
+
+fn pread(
+    _file: &kernel::bindings::file,
+    mut user_slice: UserSliceWriter,
+    offset: &kernel::bindings::loff_t,
+) -> Result<(usize, usize)> {
+    let Ok(offset) = usize::try_from(*offset) else {
+        return Err(EINVAL);
+    };
+    with_data(move |inner| {
+        let len = user_slice.len();
+        pr_info!("Wants read max {len} bytes at offset={offset}\n");
+        let cur: &[u8] = inner.buf.as_slice();
+        let Some(wants_section) = cur.get(offset..) else {
+            // EOF
+            return Ok((0, offset));
+        };
+        if len >= wants_section.len() {
+            user_slice.write_slice(wants_section)?;
+            Ok((wants_section.len(), offset + wants_section.len()))
+        } else {
+            user_slice.write_slice(&wants_section[..len])?;
+            Ok((len, offset + len))
+        }
+    })
+}
+
+fn pwrite(
+    file: &kernel::bindings::file,
+    user_slice_reader: UserSliceReader,
+    offset: &kernel::bindings::loff_t,
+) -> Result<(usize, usize)> {
+    let len = user_slice_reader.len();
+    let Ok(offset) = usize::try_from(*offset) else {
+        return Err(EINVAL);
+    };
+    let offset = if file_is_append(file) {
+        None
+    } else {
+        Some(offset)
+    };
+    let next_offset = with_data(move |inner| {
+        let buf = user_slice_reader;
         let len = buf.len();
         pr_info!("Wants write {len} bytes, offset={offset:?}\n");
 
@@ -97,139 +186,8 @@ impl SharedRamFile {
         for _byte in cur.drain(offset..) {}
         buf.read_all(cur, GFP_KERNEL)?;
         Ok(cur.len())
-    }
+    })?;
 
-    fn read(&self, buf: UserSliceWriter, offset: usize) -> Result<(usize, usize)> {
-        let shared_ram = self.lock_buf_inner()?;
-        let Some(inner) = shared_ram.as_ref() else {
-            return Err(EBUSY);
-        };
-        let mut buf = buf;
-        pr_info!("Wants read max {} bytes at offset={offset}\n", buf.len());
-        let cur: &[u8] = inner.buf.as_slice();
-        let Some(wants_section) = cur.get(offset..) else {
-            // EOF
-            return Ok((0, offset));
-        };
-        if buf.len() >= wants_section.len() {
-            buf.write_slice(wants_section)?;
-            Ok((wants_section.len(), offset + wants_section.len()))
-        } else {
-            buf.write_slice(&wants_section[..buf.len()])?;
-            Ok((buf.len(), offset + buf.len()))
-        }
-    }
-
-    fn lseek(
-        &self,
-        cur_offset: kernel::bindings::loff_t,
-        offset: kernel::bindings::loff_t,
-        whence: Whence,
-    ) -> Result<usize> {
-        let shared_ram = self.lock_buf_inner()?;
-        let Some(inner): Option<&SharedRamInnner> = shared_ram.as_ref() else {
-            return Err(EBUSY);
-        };
-        match whence {
-            Whence::SeekSet | Whence::SeekData => {
-                let Ok(offset) = usize::try_from(offset) else {
-                    return Err(EINVAL);
-                };
-                if inner.buf.len() >= offset {
-                    Ok(offset)
-                } else {
-                    Err(EINVAL)
-                }
-            }
-            Whence::SeekCur => {
-                let Ok(offset) = usize::try_from(cur_offset + offset) else {
-                    return Err(EINVAL);
-                };
-                if inner.buf.len() >= offset {
-                    Ok(offset)
-                } else {
-                    Err(EINVAL)
-                }
-            }
-            Whence::SeekEnd => {
-                let Ok(offset) =
-                    usize::try_from(inner.buf.len() as kernel::bindings::loff_t + offset)
-                else {
-                    return Err(EINVAL);
-                };
-                if inner.buf.len() >= offset {
-                    Ok(offset)
-                } else {
-                    Err(EINVAL)
-                }
-            }
-            Whence::SeekHole => Ok(inner.buf.len()),
-        }
-    }
-
-    fn lock_buf_inner(&self) -> Result<Guard<'_, Option<SharedRamInnner>, MutexBackend>> {
-        if !INITIALIZED.load(core::sync::atomic::Ordering::Acquire) {
-            return Err(EBUSY);
-        }
-        let data = unsafe { self.data.get().as_ref().unwrap() };
-        let Some(data_present) = data else {
-            return Err(EBUSY);
-        };
-        let shared_ram = data_present.lock();
-        Ok(shared_ram)
-    }
-}
-
-struct SharedRamInnner {
-    buf: alloc::vec::Vec<u8>,
-    _pde: ProcDirEntry<'static>,
-}
-
-struct ProcHand;
-
-impl ProcHandler<'static> for ProcHand {
-    const OPEN: kernel::proc_fs::ProcOpen<'static> = &popen;
-
-    const READ: kernel::proc_fs::ProcRead<'static> = &pread;
-
-    const WRITE: kernel::proc_fs::ProcWrite<'static> = &pwrite;
-
-    const LSEEK: kernel::proc_fs::ProcLseek<'static> = &plseek;
-}
-
-#[inline]
-fn popen(_inode: &kernel::bindings::inode, _file: &kernel::bindings::file) -> Result<i32> {
-    Ok(0)
-}
-
-fn pread(
-    _file: &kernel::bindings::file,
-    user_slice: UserSliceWriter,
-    offset: &kernel::bindings::loff_t,
-) -> Result<(usize, usize)> {
-    let Ok(offset) = usize::try_from(*offset) else {
-        return Err(EINVAL);
-    };
-    let (read, next_offset) = FILE_DATA.read(user_slice, offset)?;
-    Ok((read, next_offset))
-}
-
-fn pwrite(
-    file: &kernel::bindings::file,
-    user_slice_reader: UserSliceReader,
-    offset: &kernel::bindings::loff_t,
-) -> Result<(usize, usize)> {
-    let len = user_slice_reader.len();
-    let Ok(offset) = usize::try_from(*offset) else {
-        return Err(EINVAL);
-    };
-    let offset = if file_is_append(file) {
-        None
-    } else {
-        Some(offset)
-    };
-
-    let next_offset = FILE_DATA.write(user_slice_reader, offset)?;
     Ok((len, next_offset))
 }
 
@@ -238,41 +196,45 @@ fn plseek(
     offset: kernel::bindings::loff_t,
     whence: Whence,
 ) -> Result<kernel::bindings::loff_t> {
-    let off = FILE_DATA.lseek(file.f_pos, offset, whence)?;
+    let off = with_data(|inner| match whence {
+        Whence::SeekSet | Whence::SeekData => {
+            let Ok(offset) = usize::try_from(offset) else {
+                return Err(EINVAL);
+            };
+            if inner.buf.len() >= offset {
+                Ok(offset)
+            } else {
+                Err(EINVAL)
+            }
+        }
+        Whence::SeekCur => {
+            let Ok(offset) = usize::try_from(file.f_pos + offset) else {
+                return Err(EINVAL);
+            };
+            if inner.buf.len() >= offset {
+                Ok(offset)
+            } else {
+                Err(EINVAL)
+            }
+        }
+        Whence::SeekEnd => {
+            let Ok(offset) = usize::try_from(inner.buf.len() as kernel::bindings::loff_t + offset)
+            else {
+                return Err(EINVAL);
+            };
+            if inner.buf.len() >= offset {
+                Ok(offset)
+            } else {
+                Err(EINVAL)
+            }
+        }
+        Whence::SeekHole => Ok(inner.buf.len()),
+    })?;
     let Ok(output) = kernel::bindings::loff_t::try_from(off) else {
         // Todo: Should be EOVERFLOW afaik
         return Err(EINVAL);
     };
     Ok(output)
-}
-
-static INITIALIZED: AtomicBool = AtomicBool::new(false);
-static FILE_DATA: SharedRamFile = SharedRamFile::uninit();
-
-const POPS: ProcOps<'static, ProcHand> = ProcOps::<'static, ProcHand>::new(0);
-
-impl kernel::Module for RustProcRamFile {
-    fn init(_module: &'static ThisModule) -> Result<Self> {
-        let pde = proc_create(c_str!("rust-proc-file"), 0666, None, &POPS)?;
-
-        let sri = SharedRamInnner {
-            buf: alloc::vec::Vec::new(),
-            _pde: pde,
-        };
-        let lock = kernel::new_mutex!(Some(sri), "proc_ram_mutex");
-        let m = Box::pin_init(lock, GFP_KERNEL)?;
-        unsafe {
-            FILE_DATA.init(m);
-        }
-        pr_info!("Loaded /proc/rust-proc-file\n");
-        Ok(Self)
-    }
-}
-
-impl Drop for RustProcRamFile {
-    fn drop(&mut self) {
-        let _ = FILE_DATA.disable();
-    }
 }
 
 #[inline]
